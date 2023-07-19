8609bf076d38ce4ee64c71c9900d0b00f9c18892
Usama Zahid <usamabutt8080@gmail.com>
2023-07-19 11:52:40 +0500

    project added

---
+++ script.js
@@ -0,0 +1,17 @@
+function job(delay) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            console.log('Resolving', delay);
+            resolve('done ' + delay);
+        }, delay);
+    });
+}
+
+var promise = Promise.all([job(1000), job(2000), job(500), job(1500)]);
+
+promise.then(function(data) {
+    console.log('All done');
+    data.forEach((data)=>{
+        console.log(data);
+    })
+});
